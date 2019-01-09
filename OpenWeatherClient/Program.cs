79395cc571b8a0fc7de70e72ec5e12aeb04941eb
James <JamesStumme@gmail.com>
2019-01-08 21:46:36 -0600

    Initial project setup

---
+++ OpenWeatherClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenWeatherClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
